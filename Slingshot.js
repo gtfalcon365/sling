7c283079e7025c6eb9934e42b70b742205dfff8e
gtfalcon365 <75933193+gtfalcon365@users.noreply.github.com>
2021-04-05 11:10:12 +0100

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -36,7 +36,7 @@ class SlingShot{
                 strokeWeight(4);
                 line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
                 line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
-                image(this.sling3,pointA.x-30,pointA.y-10,15,30)
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30)
 
             }
             
